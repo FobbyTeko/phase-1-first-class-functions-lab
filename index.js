f49822f5fb90078187a3f91533ea9532720a85bc
Tiri Tuatai <tiri.jnr@gmail.com>
2024-02-21 21:19:12 +0800

    FC Functions Lab Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(driver) {
+    return [driver[0], driver[1]]
+}
+
+const returnLastTwoDrivers = function(driver) {
+    return [driver[2], driver[3]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (value) {
+    return function (fare) {
+        return fare * value
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers (drivers, scuber){
+    if(scuber === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(drivers)
+    } else if (scuber === returnLastTwoDrivers){
+        return returnLastTwoDrivers(drivers)
+    }
+}
